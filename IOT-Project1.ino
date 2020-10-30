f84a82a65365c07d7e467c076eb9d6b9cbe8d6ef
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2020-10-30 19:44:05 +0100

    RC1, project completed

--- IOT-Project1.ino
+++ IOT-Project1.ino
@@ -7,7 +7,22 @@
 #define PLACE_TWO PIN3
 #define PLACE_THREE PIN4
 #define PLACE_FOUR PIN5
-#define PIN_RED 7
+#define PIN_RED PIN6
+
+#define BUTTON_ONE 9
+#define BUTTON_TWO 10
+#define BUTTON_THREE 11
+#define BUTTON_FOUR 12
+
+#define START_GAME 0
+#define GAME_ON 1
+#define WAIT_INPUT 2
+
+#define TMAX 8000
+
+#define EI_ARDUINO_INTERRUPTED_PIN // to enable pin states functionality 
+#include <EnableInterrupt.h>
+
 
 class Fly {
   private:
@@ -23,9 +38,16 @@ class Fly {
       this->place = place;
     }
 
+    void fly() {
+      if (random(2)) {
+        this->goRight();
+      } else {
+        this->goLeft();
+      }
+    }
+
     void goRight() {
       this->place++;
-
       if (this->place > this->maximum) {
         this->place = this->minimum;
       }
@@ -62,7 +84,7 @@ class StepAnalogReader {
 
     int getLevel() {
       int read = analogRead(this->pin);
-      return abs(read / ((minimum - maximum) / steps));
+      return abs(read / ((minimum - maximum) / steps)) + 1;
     }
 };
 
@@ -70,9 +92,20 @@ class StepAnalogReader {
 
 int places[] = {PLACE_ONE, PLACE_TWO, PLACE_THREE, PLACE_FOUR};
 int placesSize = sizeof(places) / sizeof(places[0]);
-int currentPlace;
+int buttons[] = {BUTTON_ONE, BUTTON_TWO, BUTTON_THREE, BUTTON_FOUR};
+int buttonsSize = sizeof(buttons) / sizeof(buttons[0]);
+
+int score;
+int gameStat = START_GAME;
+unsigned long maxTime = 0;
+unsigned long flyStops = 0;
+
+int fadeStep = 5;
+int brightness = 0;
+
 Fly fly;
 StepAnalogReader stepAnalogReader (DIFFICULTY_PIN, DIFFICULTY_STEPS);
+volatile uint8_t interruptedPinShared;
 
 int current;
 
@@ -80,30 +113,62 @@ void setup() {
   Serial.begin(9600);
   randomSeed(analogRead(0));
 
-  currentPlace = random(0, placesSize);
-  fly.setUp(0, placesSize, currentPlace);
+  fly.setUp(0, placesSize, random(0, placesSize));
 
   initLeds();
   if (DEBUG) {
     testLeds();
   }
+  initButtons();
+ 
+  Serial.println("Welcome to the Track to Led Fly Game. Press Key T1 to Start.");
 }
 
 void loop() {
-  //  bool goRight = random(2);
-  //  if (goRight) {
-  //    fly.goRight();
-  //  } else {
-  //    fly.goLeft();
-  //  }
-  //  Serial.println("Tracking the fly: pos " + String(fly.getPlace()));
-  //  digitalWrite(places[fly.getPlace()], HIGH);
-  //  delay(500);
-  //  digitalWrite(places[fly.getPlace()], LOW);
-  if (stepAnalogReader.getLevel() != current) {
-    current = stepAnalogReader.getLevel();
-    Serial.println(current);
+  static uint8_t interruptedPin;
+  noInterrupts();
+  interruptedPin = interruptedPinShared;
+  interruptedPinShared = 0;
+  interrupts();
+  switch (gameStat) {
+    case START_GAME:
+      analogWrite(PIN_RED, brightness);
+      brightness = brightness + fadeStep;
+      if (brightness <= 0 || brightness >= 255) {
+        fadeStep = -fadeStep;
+      }
+      delay(30);
+      if (interruptedPin == buttons[0]) {
+        gameStat = GAME_ON;
+        maxTime = TMAX / stepAnalogReader.getLevel();
+        score = 0;
+        Serial.println("Go!");
+        analogWrite(PIN_RED, 0);
+      }
+      break;
+    case GAME_ON:
+      fly.fly();
+      gameStat = WAIT_INPUT;
+      digitalWrite(places[fly.getPlace()], HIGH);
+      flyStops = millis();
+    case WAIT_INPUT:
+      if (millis() - flyStops > maxTime || (interruptedPin != buttons[fly.getPlace()] && interruptedPin != 0)) {
+        Serial.println("Game Over - Score: " + String(score));
+        gameStat = START_GAME;
+        digitalWrite(places[fly.getPlace()], LOW);
+      } else if (interruptedPin == buttons[fly.getPlace()]) {
+        gameStat = GAME_ON;
+        Serial.println("Tracking the fly: pos " + String(fly.getPlace() + 1));
+        score++;
+        digitalWrite(places[fly.getPlace()], LOW);
+        maxTime = maxTime/8*7;
+        if (DEBUG) {
+          printDebug("maxTime decreasing! Now is: " + String(maxTime));
+        }
+      }
+      break;
   }
+  interruptedPin = 0;
 }
 
 void initLeds() {
@@ -128,6 +193,16 @@ void testLeds() {
   digitalWrite(PIN_RED, LOW);
 }
 
+void initButtons() {
+  for (int i = 0; i < buttonsSize; i++) {
+    enableInterrupt(buttons[i], clickEvent, RISING);
+  }
+}
+
 void printDebug(String text) {
   Serial.println("DEBUG: " + text);
+}
+
+void clickEvent() {
+  interruptedPinShared = arduinoInterruptedPin;
 }
